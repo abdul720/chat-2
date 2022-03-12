a220797049c8756d93ccf9722a554800a9af6037
abdul720 <73010648+abdul720@users.noreply.github.com>
2022-03-12 05:18:10 -1200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+import Card from './components.js/Card';
+
+function App() {
+  return (
+    <Card />
+  );
+}
+
+export default App;
